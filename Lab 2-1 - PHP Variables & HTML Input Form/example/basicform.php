57cf7468ab7b74dbe28b151b1cf152b0f0e42a94
matdieuhau999 <matdieuhau999@gmail.com>
2021-04-01 12:40:44 +0700

    example

---
+++ Lab 2-1 - PHP Variables & HTML Input Form/example/basicform.php
@@ -0,0 +1,12 @@
+<HTML>
+    <HEAD><TITLE> A Simple Form </TITLE></HEAD>
+    <BODY>
+        <FORM action="http://fit.hut.edu.vn/~trangntt/course/wp" METHOD=POST>
+            Click submit to start our initial PHP program.
+            <BR>
+            <input  tyoe="text" name = "Name">
+            <BR>
+            <INPUT TYPE="SUBMIT" VALUE="Click To Submit">
+            <INPUT TYPE="RESET" VALUE="Erase and Restart">
+        </FROM>
+    </BODY>
