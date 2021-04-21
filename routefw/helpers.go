689c6609bd130334fdb79809a9623ed61f4c64ee
matdieuhau999 <matdieuhau999@gmail.com>
2021-04-21 18:28:58 +0700

    update

---
+++ routefw/helpers.go
@@ -0,0 +1,75 @@
+package routefw
+
+import (
+	"path"
+)
+
+func joinPaths(absolutePath, relativePath string) string {
+	if relativePath == "" {
+		return absolutePath
+	}
+
+	finalPath := path.Join(absolutePath, relativePath)
+	appendSlash := lastChar(relativePath) == '/' && lastChar(finalPath) != '/'
+	if appendSlash {
+		return finalPath + "/"
+	}
+	return finalPath
+}
+
+func lastChar(str string) uint8 {
+	if str == "" {
+		panic("The length of the string can't be 0")
+	}
+	return str[len(str)-1]
+}
+
+func countParams(path string) uint8{
+	var n int
+	for i:=0; i < len(path); i++{
+		if path[i] == ':'{
+			n++
+		}
+
+	}
+	if n >= 255 {
+		panic("too many param")
+	}
+	return uint8(n)
+}
+
+func findWildcard(path string) (wildcard string, i int, valid bool){
+	for start, c := range []byte(path){
+		if c != ':' {
+			continue
+		}
+
+		valid = true
+		for end, c := range []byte(path[start+1:]){
+			switch c {
+			case '/':
+				return path[start: start+1+end], start, valid
+			case ':':
+				valid = false
+			}
+		}
+		return path[start:], start, valid
+	}
+	return "", -1, false
+}
+
+
+
+func longestCommonPrefix(a, b string) int{
+	i := 0
+	max := 0
+	if len(a) <= len(b){
+		max = len(a)
+	}else{
+		max = len(b)
+	}
+	for i < max && a[i] == b[i]{
+		i++
+	}
+	return i
+}
--- routefw/node.go
+++ routefw/node.go
@@ -2,6 +2,7 @@ package routefw
 
 import (
 	"net/url"
+	"strings"
 )
 
 const (
@@ -16,7 +17,7 @@ type node struct{
 	indices   string
 	children  []*node
 	handler HandlerFunc
-	//priority  uint32
+	priority  uint32
 	nType     nodeType
 	maxParams uint8
 	wildChild bool
@@ -122,6 +123,215 @@ walk:
 	}
 }
 
-func (n *node) addRoute(path string, handler HandlerFunc){
+func (n *node) insertChild(numParams uint8, path string, fullPath string, handler HandlerFunc){
+	for numParams > 0 {
+		wildcard, i, valid := findWildcard(path)
+		if i < 0{
+			break
+		}
 
+		if !valid || len(wildcard) < 2{
+			panic("path " + path + " error")
+		}
+		// example /web/:abc and /web/:def
+		if len(n.children) > 0{
+			panic(wildcard + " conflic with exist children in " + fullPath)
+		}
+		//handle param
+		if wildcard[0] == ':' {
+			if i > 0{
+				n.path = path[:i]
+				path = path[i:]
+			}
+
+			n.wildChild = true
+			child := &node{
+				nType:		param,
+				path:		wildcard,
+				maxParams: 	numParams,
+				fullPath: 	fullPath,
+			}
+			n.children = []*node{child}
+			n = child
+			n.priority++
+			numParams--
+
+			if len(wildcard) < len(path){
+				path = path[len(wildcard):]
+				child := &node{
+					maxParams: numParams,
+					priority: 1,
+					fullPath: fullPath,
+				}
+				n.children = []*node{child}
+				n = child
+				continue
+			}
+			n.handler = handler
+		}
+
+		i--
+		n.path = path[:i]
+		child := &node{
+			wildChild: true,
+			nType: catchAll,
+			maxParams: 1,
+			fullPath: fullPath,
+		}
+
+		if n.maxParams < 1{
+			n.maxParams = 1
+		}
+
+		n.children = []*node{child}
+		n.indices = string('/')
+		n = child
+		n.priority++
+		child = &node{
+			path: path[i:],
+			nType: catchAll,
+			maxParams: 1,
+			handler: handler,
+			priority: 1,
+			fullPath: fullPath,
+		}
+		n.children = []*node{child}
+		return
+	}
+
+	//no param
+	n.path = path
+	n.handler = handler
+	n.fullPath = fullPath
 }
+
+func (n *node) incrementChildPrio(pos int) int {
+	cs := n.children
+	cs[pos].priority++
+	prio := cs[pos].priority
+
+	newPos := pos
+
+	for ;newPos > 0 && cs[newPos-1].priority < prio; newPos--{
+		cs[newPos-1], cs[newPos] = cs[newPos], cs[newPos-1]
+	}
+
+	if newPos != pos{
+		n.indices = n.indices[:newPos] +
+			n.indices[pos:pos+1] +
+			n.indices[newPos:pos] + n.indices[pos+1:]
+	}
+	return newPos
+}
+
+func (n *node) addRoute(path string, handler HandlerFunc){
+	fullPath := path
+	numParams := countParams(path)
+	n.priority++
+	//empty tree
+	if len(n.path) == 0 && len(n.children) == 0{
+		n.insertChild(numParams, path, fullPath, handler)
+		n.nType = root
+		return
+	}
+	parentFullPathIndex := 0
+
+walk:
+	for{
+		if numParams > n.maxParams{
+			n.maxParams = numParams
+		}
+		i := longestCommonPrefix(path, n.path)
+
+		if i < len(n.path){
+			child := node{
+				path: n.path[i:],
+				wildChild: n.wildChild,
+				indices: n.indices,
+				children: n.children,
+				handler: n.handler,
+				priority: n.priority-1,
+				fullPath: n.fullPath,
+			}
+
+			for _, v := range child.children{
+				if v.maxParams > child.maxParams{
+					child.maxParams = v.maxParams
+				}
+			}
+			n.children = []*node{&child}
+			n.indices = string([]byte{n.path[i]})
+			n.path = path[:i]
+			n.handler = nil
+			n.wildChild = false
+			n.fullPath = fullPath[:parentFullPathIndex+i]
+		}
+
+		if i < len(path){
+			path = path[i:]
+			if n.wildChild{
+				parentFullPathIndex += len(n.path)
+				n = n.children[0]
+				n.priority++
+
+				if numParams > n.maxParams{
+					n.maxParams = numParams
+				}
+				numParams--
+
+				if len(path) >= len(n.path) && n.path == path[:len(n.path)]{
+					if len(n.path) >= len(path) && path[len(n.path)] == '/'{
+						continue walk
+					}
+				}
+
+				pathSeg := path
+				if n.nType != catchAll{
+					pathSeg = strings.SplitN(path, "/", 2)[0]
+				}
+				prefix := fullPath[:strings.Index(fullPath, pathSeg)] + n.path
+				panic("'" + pathSeg +
+					"' in new path '" + fullPath +
+					"' conflicts with existing wildcard '" + n.path +
+					"' in existing prefix '" + prefix +
+					"'")
+			}
+			c := path[0]
+			if n.nType == param && c == '/' && len(n.children) == 1{
+				parentFullPathIndex += len(n.path)
+				n = n.children[0]
+				n.priority++
+				continue walk
+			}
+			for i:= 0; i < len(n.indices); i++{
+				if c == n.indices[i]{
+					parentFullPathIndex += len(n.path)
+					i = n.incrementChildPrio(i)
+					n = n.children[i]
+					continue walk
+				}
+			}
+
+			if c!= ':'{
+				n.indices += string([]byte{c})
+				child := &node{
+					maxParams: numParams,
+					fullPath: fullPath,
+				}
+				n.children = append(n.children, child)
+				n.incrementChildPrio(len(n.indices) - 1)
+				n = child
+			}
+
+			n.insertChild(numParams, path, fullPath, handler)
+			return
+		}
+
+		if n.handler != nil{
+			panic("handlers are already registered for path '" + fullPath + "'")
+		}
+		n.handler = handler
+		return
+	}
+}
+
